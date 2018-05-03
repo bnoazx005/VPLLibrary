86955de25078b195fed815753f933a4326be08fd
bnoazx005 <ildar2571@yandex.ru>
2018-05-03 18:18:51 +0400

    Add unit tests for GetHead method of CIntrinsicsUtils class
    Changes:
    * Three unit tests were added for GetHead method of CIntrinsicsUtils
    class.

---
+++ VPLLibraryTests/Tests/CIntrinsicsUtilsTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using VPLLibrary.Impls;
+
+namespace VPLLibraryTests.Tests
+{
+    [TestFixture]
+    public class CIntrinsicsUtilsTests
+    {
+        [Test]
+        public void TestGetHead_CorrectArrayWith4Elements_ReturnsFirstElement()
+        {
+            int[] testArray = new int[4] { 42, 23, -9, 12 };
+
+            int[] resultArray = null;
+
+            Assert.DoesNotThrow(() => { resultArray = CIntrinsicsUtils.GetHead(testArray); });
+
+            Assert.AreEqual(resultArray.Length, 1);
+            Assert.AreEqual(resultArray[0], testArray[0]);
+        }
+
+        [Test]
+        public void TestGetHead_EmptyArray_ReturnsThisArray()
+        {
+            int[] testArray = new int[0];
+
+            int[] resultArray = null;
+
+            Assert.DoesNotThrow(() => { resultArray = CIntrinsicsUtils.GetHead(testArray); });
+
+            Assert.AreSame(resultArray, testArray);
+        }
+
+        [Test]
+        public void TestGetHead_PassNullArgument_ThrowsException()
+        {            
+            Assert.Throws<CRuntimeError>(() => { CIntrinsicsUtils.GetHead(null); });            
+        }
+    }
+}
