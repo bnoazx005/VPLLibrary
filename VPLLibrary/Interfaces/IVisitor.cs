9ac7625d510a07e2a290cabbc4106b697a56f44e
bnoazx005 <ildar2571@yandex.ru>
2018-05-03 14:55:15 +0400

    Expand basic functionality of IVisitor
    Changes:
    * A few methods were introduced into IVisitor: visitProgramNode,
    visitAssigmentNode, visitIdentifierNode and visitValueNode.

--- VPLLibrary/Interfaces/IVisitor.cs
+++ VPLLibrary/Interfaces/IVisitor.cs
@@ -2,5 +2,12 @@
 {
     public interface IVisitor<T>
     {
+        T visitProgramNode(IASTNode program);
+
+        T visitAssigmentNode(IAssigmentASTNode assigment);
+
+        T visitIdentifierNode(IIdentifierASTNode identifier);
+
+        T visitValueNode(IValueASTNode value);
     }
 }
