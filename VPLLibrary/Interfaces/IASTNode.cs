161cec0c8334408aa5fef85a40537c47a5ee561b
bnoazx005 <ildar2571@yandex.ru>
2018-05-03 12:08:27 +0400

    Add new types of interfaces
    Changes:
    * A new interface IInterpreter was added.
    * A new interface IASTNode was added.

---
+++ VPLLibrary/Interfaces/IASTNode.cs
@@ -0,0 +1,28 @@
+﻿namespace VPLLibrary.Interfaces
+{
+    /// <summary>
+    /// The enumeration represents all types of AST nodes
+    /// </summary>
+
+    public enum E_NODE_TYPE
+    {
+        NT_PROGRAM,
+        NT_EXPRESSION,
+    }
+
+
+    /// <summary>
+    /// interface IASTNode
+    /// 
+    /// The interface represents a single AST node's functionality
+    /// </summary>
+
+    public interface IASTNode
+    {
+        /// <summary>
+        /// The readonly property returns a type of a node
+        /// </summary>
+
+        E_NODE_TYPE Type { get; }
+    }
+}
---
+++ VPLLibrary/Interfaces/IInterpreter.cs
@@ -0,0 +1,22 @@
+﻿namespace VPLLibrary.Interfaces
+{
+    /// <summary>
+    /// interface IInterpreter
+    /// 
+    /// The class provides functionality of AST's execution for
+    /// the DSL
+    /// </summary>
+
+    public interface IInterpreter
+    {
+        /// <summary>
+        /// The method evaluates (executes) a program, which is represented as a syntax tree of nodes.
+        /// An input parameters could be integer arrays or integer values (represented as an array with single value).
+        /// </summary>
+        /// <param name="program">A program is represented as an AST</param>
+        /// <param name="inputData">Program's input data</param>
+        /// <returns>An evaluated array of some data</returns>
+
+        int[] Eval(IASTNode program, int[][] inputData);
+    }
+}
