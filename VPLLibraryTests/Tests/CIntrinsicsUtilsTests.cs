24bca302f963b0976d0e32141aef46c8ba41d8e6
bnoazx005 <ildar2571@yandex.ru>
2018-05-03 18:21:26 +0400

    Add unit tests for GetLast method of CIntrinsicsUtils
    Changes:
    * Three unit tests were added for GetLast of CIntrinsicsUtils.

--- VPLLibraryTests/Tests/CIntrinsicsUtilsTests.cs
+++ VPLLibraryTests/Tests/CIntrinsicsUtilsTests.cs
@@ -36,5 +36,36 @@ namespace VPLLibraryTests.Tests
         {            
             Assert.Throws<CRuntimeError>(() => { CIntrinsicsUtils.GetHead(null); });            
         }
+
+        [Test]
+        public void TestGetLast_CorrectArrayWith4Elements_ReturnsLastElement()
+        {
+            int[] testArray = new int[4] { 42, 23, -9, 12 };
+
+            int[] resultArray = null;
+
+            Assert.DoesNotThrow(() => { resultArray = CIntrinsicsUtils.GetLast(testArray); });
+
+            Assert.AreEqual(resultArray.Length, 1);
+            Assert.AreEqual(resultArray[0], testArray[testArray.Length - 1]);
+        }
+
+        [Test]
+        public void TestGetLast_EmptyArray_ReturnsThisArray()
+        {
+            int[] testArray = new int[0];
+
+            int[] resultArray = null;
+
+            Assert.DoesNotThrow(() => { resultArray = CIntrinsicsUtils.GetLast(testArray); });
+
+            Assert.AreSame(resultArray, testArray);
+        }
+
+        [Test]
+        public void TestGetLast_PassNullArgument_ThrowsException()
+        {
+            Assert.Throws<CRuntimeError>(() => { CIntrinsicsUtils.GetLast(null); });
+        }
     }
 }
